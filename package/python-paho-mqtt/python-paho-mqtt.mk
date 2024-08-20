0266142d67e9399f774b98d66b1ff5bee95edd14
Mauricio Vidal <maovidal@users.noreply.github.com>
2024-08-20 16:28:44 -0500

    bump: packages versions via req_updater

--- package/python-paho-mqtt/python-paho-mqtt.mk
+++ package/python-paho-mqtt/python-paho-mqtt.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PYTHON_PAHO_MQTT_VERSION = 2.0.0
-PYTHON_PAHO_MQTT_SITE = $(call github,eclipse,paho.mqtt.python,v$(PYTHON_PAHO_MQTT_VERSION))
+PYTHON_PAHO_MQTT_VERSION = 2.1.0
+PYTHON_PAHO_MQTT_SITE = https://files.pythonhosted.org/packages/39/15/0a6214e76d4d32e7f663b109cf71fb22561c2be0f701d67f93950cd40542
 PYTHON_PAHO_MQTT_LICENSE = EPL-2.0, EDLv1.0
 PYTHON_PAHO_MQTT_LICENSE_FILES = LICENSE.txt epl-v20 edl-v10
 PYTHON_PAHO_MQTT_SETUP_TYPE = pep517
