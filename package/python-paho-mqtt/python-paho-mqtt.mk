bf20425dfaa413594769760197fc8f994e89d49c
Mauricio Vidal <maovidal@users.noreply.github.com>
2024-08-20 19:01:33 -0500

    fix: python-paho-mqtt

--- package/python-paho-mqtt/python-paho-mqtt.mk
+++ package/python-paho-mqtt/python-paho-mqtt.mk
@@ -5,6 +5,7 @@
 ################################################################################
 
 PYTHON_PAHO_MQTT_VERSION = 2.1.0
+PYTHON_PAHO_MQTT_SOURCE = paho_mqtt-$(PYTHON_PAHO_MQTT_VERSION).tar.gz
 PYTHON_PAHO_MQTT_SITE = https://files.pythonhosted.org/packages/39/15/0a6214e76d4d32e7f663b109cf71fb22561c2be0f701d67f93950cd40542
 PYTHON_PAHO_MQTT_LICENSE = EPL-2.0, EDLv1.0
 PYTHON_PAHO_MQTT_LICENSE_FILES = LICENSE.txt epl-v20 edl-v10
