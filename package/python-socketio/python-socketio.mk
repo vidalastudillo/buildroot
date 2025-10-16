0d080af2d0884e19a9322cf7649df4d24e499dff
Mauricio Vidal <13444566+maovidal@users.noreply.github.com>
2025-10-16 10:36:23 -0500

    update:  package via `req_updater`

--- package/python-socketio/python-socketio.mk
+++ package/python-socketio/python-socketio.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_SOCKETIO_VERSION = 5.13.0
+PYTHON_SOCKETIO_VERSION = 5.14.1
 PYTHON_SOCKETIO_SOURCE = python_socketio-$(PYTHON_SOCKETIO_VERSION).tar.gz
-PYTHON_SOCKETIO_SITE = https://files.pythonhosted.org/packages/21/1a/396d50ccf06ee539fa758ce5623b59a9cb27637fc4b2dc07ed08bf495e77
+PYTHON_SOCKETIO_SITE = https://files.pythonhosted.org/packages/05/c2/a9ae3d0eb4488748a2d9c15defddb7277a852234e29e50c73136834dff1b
 PYTHON_SOCKETIO_SETUP_TYPE = setuptools
 PYTHON_SOCKETIO_LICENSE = MIT
 PYTHON_SOCKETIO_LICENSE_FILES = LICENSE
