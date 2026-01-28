d3f6ffc46e9374db83adbfae51ef173feda7e238
Mauricio Vidal <13444566+maovidal@users.noreply.github.com>
2026-01-28 14:12:15 -0500

    fix: package `python-pydantic-core` dependency

--- package/python-pydantic-core/python-pydantic-core.mk
+++ package/python-pydantic-core/python-pydantic-core.mk
@@ -8,10 +8,11 @@
 # make sure to sync pydantic-core to this version when updating pydantic
 PYTHON_PYDANTIC_CORE_VERSION = 2.41.5
 PYTHON_PYDANTIC_CORE_SOURCE_PYPI = pydantic_core-$(PYTHON_PYDANTIC_CORE_VERSION).tar.gz
-PYTHON_PYDANTIC_CORE_SITE = https://files.pythonhosted.org/packages/71/70/23b021c950c2addd24ec408e9ab05d59b035b39d97cdc1130e1bce647bb6
-PYTHON_PYDANTIC_CORE_SITE_PYPI = https://files.pythonhosted.org/packages/df/18/d0944e8eaaa3efd0a91b0f1fc537d3be55ad35091b6a87638211ba691964
+PYTHON_PYDANTIC_CORE_SITE = $(PYTHON_PYDANTIC_CORE_SITE_PYPI)/$(PYTHON_PYDANTIC_CORE_SOURCE_PYPI)?buildroot-path=filename
+PYTHON_PYDANTIC_CORE_SITE_PYPI = https://files.pythonhosted.org/packages/71/70/23b021c950c2addd24ec408e9ab05d59b035b39d97cdc1130e1bce647bb6
 PYTHON_PYDANTIC_CORE_SETUP_TYPE = maturin
 PYTHON_PYDANTIC_CORE_LICENSE = MIT
 PYTHON_PYDANTIC_CORE_LICENSE_FILES = LICENSE
+PYTHON_PYDANTIC_CORE_DEPENDENCIES = host-python-typing-extensions
 
 $(eval $(python-package))
