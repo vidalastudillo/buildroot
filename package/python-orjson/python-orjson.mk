07905f21142e2635e29969edaf54a01b7cc98b3c
Mauricio Vidal <13444566+maovidal@users.noreply.github.com>
2026-02-05 14:21:10 -0500

    draft: python-orjson

--- package/python-orjson/python-orjson.mk
+++ package/python-orjson/python-orjson.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_ORJSON_VERSION = 3.11.4
+PYTHON_ORJSON_VERSION = 3.11.7
 PYTHON_ORJSON_SOURCE = orjson-$(PYTHON_ORJSON_VERSION).tar.gz
-PYTHON_ORJSON_SITE = https://files.pythonhosted.org/packages/source/o/orjson
+PYTHON_ORJSON_SITE = https://files.pythonhosted.org/packages/53/45/b268004f745ede84e5798b48ee12b05129d19235d0e15267aa57dcdb400b
 PYTHON_ORJSON_SETUP_TYPE = maturin
 PYTHON_ORJSON_BUILD_TARGET = wheel
 PYTHON_ORJSON_LICENSE = Apache-2.0 or MIT
