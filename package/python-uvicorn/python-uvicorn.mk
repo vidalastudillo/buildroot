629182dd9c11021b9871bde4a0c28ecdbaea6890
Mauricio Vidal <13444566+maovidal@users.noreply.github.com>
2026-04-06 10:32:49 -0500

    Updated packages via `req_updater`

--- package/python-uvicorn/python-uvicorn.mk
+++ package/python-uvicorn/python-uvicorn.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_UVICORN_VERSION = 0.43.0
+PYTHON_UVICORN_VERSION = 0.44.0
 PYTHON_UVICORN_SOURCE = uvicorn-$(PYTHON_UVICORN_VERSION).tar.gz
-PYTHON_UVICORN_SITE = https://files.pythonhosted.org/packages/62/f2/368268300fb8af33743508d738ef7bb4d56afdb46c6d9c0fa3dd515df171
+PYTHON_UVICORN_SITE = https://files.pythonhosted.org/packages/5e/da/6eee1ff8b6cbeed47eeb5229749168e81eb4b7b999a1a15a7176e51410c9
 PYTHON_UVICORN_SETUP_TYPE = hatch
 PYTHON_UVICORN_LICENSE = BSD-3-Clause
 PYTHON_UVICORN_LICENSE_FILES = LICENSE.md
