834440c971a918b3043a62626388bfc15725d4d0
Mauricio Vidal <13444566+maovidal@users.noreply.github.com>
2026-05-06 14:18:21 -0500

    package/python-pydantic: bump to version 2.13.4
    - Update python-pydantic to 2.13.4
    - Update python-pydantic-core to 2.46.4 (and sync cargo vendor tarball)
    - Update python-pydantic-settings to 2.14.0

--- package/python-pydantic/python-pydantic.mk
+++ package/python-pydantic/python-pydantic.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYDANTIC_VERSION = 2.13.3
+PYTHON_PYDANTIC_VERSION = 2.13.4
 PYTHON_PYDANTIC_SOURCE = pydantic-$(PYTHON_PYDANTIC_VERSION).tar.gz
-PYTHON_PYDANTIC_SITE = https://files.pythonhosted.org/packages/d9/e4/40d09941a2cebcb20609b86a559817d5b9291c49dd6f8c87e5feffbe703a
+PYTHON_PYDANTIC_SITE = https://files.pythonhosted.org/packages/18/a5/b60d21ac674192f8ab0ba4e9fd860690f9b4a6e51ca5df118733b487d8d6
 PYTHON_PYDANTIC_SETUP_TYPE = hatch
 PYTHON_PYDANTIC_LICENSE = MIT
 PYTHON_PYDANTIC_LICENSE_FILES = LICENSE
