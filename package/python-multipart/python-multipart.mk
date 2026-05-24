a6ff675be69ffb6a3dd14f138f9076be15f8a1ac
Mauricio Vidal <13444566+maovidal@users.noreply.github.com>
2026-05-24 09:01:07 -0500

    update: several packages via `buildroot_sync`

--- package/python-multipart/python-multipart.mk
+++ package/python-multipart/python-multipart.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_MULTIPART_VERSION = 0.0.28
+PYTHON_MULTIPART_VERSION = 0.0.29
 PYTHON_MULTIPART_SOURCE = python_multipart-$(PYTHON_MULTIPART_VERSION).tar.gz
-PYTHON_MULTIPART_SITE = https://files.pythonhosted.org/packages/82/54/a85eb421fbdd5007bc5af39d0f4ed9fa609e0fedbfdc2adcf0b34526870e
+PYTHON_MULTIPART_SITE = https://files.pythonhosted.org/packages/4e/fe/70bd71a6738b09a0bdf6480ca6436b167469ca4578b2a0efbe390b4b0e70
 PYTHON_MULTIPART_SETUP_TYPE = hatch
 PYTHON_MULTIPART_LICENSE = Apache-2.0
 PYTHON_MULTIPART_LICENSE_FILES = LICENSE.txt
--- package/python-uvicorn/python-uvicorn.mk
+++ package/python-uvicorn/python-uvicorn.mk
@@ -4,13 +4,17 @@
 #
 ################################################################################
 
-PYTHON_UVICORN_VERSION = 0.47.0
+PYTHON_UVICORN_VERSION = 0.48.0
 PYTHON_UVICORN_SOURCE = uvicorn-$(PYTHON_UVICORN_VERSION).tar.gz
-PYTHON_UVICORN_SITE = https://files.pythonhosted.org/packages/f6/b1/8e7077a8641086aea449e1b5752a570f1b5906c64e0a33cd6d93b63a066b
+PYTHON_UVICORN_SITE = https://files.pythonhosted.org/packages/e6/bf/f6544ba992ddb9a6077343a576f9844f7f8f06ab819aefd00206e9255f18
 PYTHON_UVICORN_SETUP_TYPE = hatch
 PYTHON_UVICORN_LICENSE = BSD-3-Clause
 PYTHON_UVICORN_LICENSE_FILES = LICENSE.md
 PYTHON_UVICORN_CPE_ID_VENDOR = encode
 PYTHON_UVICORN_CPE_ID_PRODUCT = uvicorn
+
+PYTHON_UVICORN_DEPENDENCIES = \
+	python-click \
+	python-h11
 
 $(eval $(python-package))
