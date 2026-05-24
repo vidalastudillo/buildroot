e16438861cf92be73e05c9683726862ccca3f60e
Mauricio Vidal <13444566+maovidal@users.noreply.github.com>
2026-05-23 22:56:26 -0500

    update: several packages via `req_updater`

--- package/python-idna/python-idna.mk
+++ package/python-idna/python-idna.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_IDNA_VERSION = 3.15
+PYTHON_IDNA_VERSION = 3.16
 PYTHON_IDNA_SOURCE = idna-$(PYTHON_IDNA_VERSION).tar.gz
-PYTHON_IDNA_SITE = https://files.pythonhosted.org/packages/82/77/7b3966d0b9d1d31a36ddf1746926a11dface89a83409bf1483f0237aa758
+PYTHON_IDNA_SITE = https://files.pythonhosted.org/packages/1a/88/bcf9709822fe69d02c2a6a77956c98ce6ea8ca8767a9aadcedc7eb6a2390
 PYTHON_IDNA_LICENSE = BSD-3-Clause
 PYTHON_IDNA_LICENSE_FILES = LICENSE.md
 PYTHON_IDNA_SETUP_TYPE = flit
