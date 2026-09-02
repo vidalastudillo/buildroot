4ebafc32e6f75ab3b32354dfe74d8947a44d10dd
Mauricio Vidal <13444566+maovidal@users.noreply.github.com>
2026-09-02 17:20:28 -0500

    Updated packages via `buildroot_sync`

--- package/python-anyio/python-anyio.mk
+++ package/python-anyio/python-anyio.mk
@@ -4,14 +4,15 @@
 #
 ################################################################################
 
-PYTHON_ANYIO_VERSION = 4.14.2
+PYTHON_ANYIO_VERSION = 4.15.0
 PYTHON_ANYIO_SOURCE = anyio-$(PYTHON_ANYIO_VERSION).tar.gz
-PYTHON_ANYIO_SITE = https://files.pythonhosted.org/packages/61/cc/a381afa6efea9f496eff839d4a6a1aed3bfafc7b3ab4b0d1b243a12573dd
+PYTHON_ANYIO_SITE = https://files.pythonhosted.org/packages/ea/9a/c15a60547004a3f3cea20296c934f827ddd7bdba225a2e7e9fcb5ec48c80
 PYTHON_ANYIO_SETUP_TYPE = setuptools
 PYTHON_ANYIO_LICENSE = MIT
 PYTHON_ANYIO_LICENSE_FILES = LICENSE
 PYTHON_ANYIO_DEPENDENCIES = \
 	python-idna \
+	python-typing-extensions \
 	host-python-setuptools-scm
 
 $(eval $(python-package))
